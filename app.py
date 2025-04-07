4fd0814fd2a5a9c3ac433011778b1a7186af50f7
OmkarSG07 <omkarsg.7781@gmail.com>
2025-04-07 14:57:03 +0530

    changes in Flask App

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, This app Flask CI/CD is created for handson practice of DevOps in realtime!"
-
+    return "Hello, This (Flask CI/CD) App is created for handson practice of DevOps in realtime!"
+ 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
